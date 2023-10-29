fac7485c7d2ef61f8170f984fb59ec981a9cbdce
issambgt <issambgt03@gmail.com>
2023-10-29 04:53:06 -0700

    a

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.5
+"""
+
+Module composed by a function that multiplies 2 matrices
+
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Function that multiplies 2 matrices
+
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+
+    Returns:
+        result of the multiplication
+
+
+    """
+
+    return (np.matmul(m_a, m_b))
+
+if__name__=="__main__";
+import doctest
+doctest.testfile("tests/101-lazy_matrix_mul.txt")
